0c993dcccd66af5de83169c4c7e6d2e1f92879b3
0-0april <kaleaamaranth@gmail.com>
2026-07-25 23:55:24 +0800

    landing

--- frontend/pages/Landing.jsx
+++ frontend/pages/Landing.jsx
@@ -146,7 +146,7 @@ export default function Landing() {
         <div
           className="absolute inset-0"
           aria-hidden="true"
-          style={{background: 'linear-gradient(to right, hsla(132, 39%, 59%, 1) 0%, hsla(132, 39%, 59%, 0.5) 40%, hsla(132, 39%, 59%, .5) 70%)',
+          style={{background: 'linear-gradient(to right, hsla(132, 39%, 59%, 1) 0%, hsla(132, 39%, 59%, 0.3) 40%, hsla(132, 39%, 59%, 0) 70%)',
             
           }}
         />
